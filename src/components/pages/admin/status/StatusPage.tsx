363996dd30479b37535041b016a13329d0d2c5a0
heehminh <mink141416@gmail.com>
2023-09-18 02:11:08 +0900

    feat: 신고내역조회, 개선요청내역조회 api 연결

--- src/components/pages/admin/status/StatusPage.tsx
+++ src/components/pages/admin/status/StatusPage.tsx
@@ -1,40 +1,35 @@
-import React from "react";
-import condition from "./mock/condition.json";
-import improvement from "./mock/improvement.json";
 import { Typography } from "@mui/material";
 import { Column } from "primereact/column";
-import { DataTable } from "primereact/datatable";
+import { useGetHistoriesImprovements, useGetHistoriesStatus } from "@/hooks/queries/statusQuries";
+import { CssDataTable } from "../components/Table";
 
 const StatusPage = () => {
-  // TODO - api 연결
   // 신고 내역 조회
-  const improvementReports = improvement.data.improvementReports;
+  const { data: conditionReportsRes } = useGetHistoriesStatus();
 
   // 개선 요청 내역 조회
-  const conditionReports = condition.data.conditionReports;
+  const { data: improvementReportsRes } = useGetHistoriesImprovements();
 
-  // TODO - UI 연결
   return (
     <div>
       <div>
-        <Typography>신고 내역 조회</Typography>
-        <DataTable value={improvementReports}>
+        <Typography>신고 내역 조회 - 대여폼</Typography>
+        <CssDataTable value={conditionReportsRes?.conditionReports}>
           <Column field="id" header="ID" />
           <Column field="umbrellaUuid" header="umbrellaUuid" />
           <Column field="content" header="content" />
           <Column field="etc" header="etc" />
-        </DataTable>
+        </CssDataTable>
       </div>
 
       <div>
-        <Typography>개선 요청 내역 조회</Typography>
-        {/* MAP */}
-        <DataTable value={conditionReports}>
+        <Typography>개선 요청 내역 조회 - 반납폼</Typography>
+        <CssDataTable value={improvementReportsRes?.improvementReports}>
           <Column field="id" header="ID" />
           <Column field="umbrellaUuid" header="umbrellaUuid" />
           <Column field="content" header="content" />
           <Column field="etc" header="etc" />
-        </DataTable>
+        </CssDataTable>
       </div>
     </div>
   );
